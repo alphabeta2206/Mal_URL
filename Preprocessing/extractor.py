5f0d5438cf1f6bc58c7a4c9aaf09e29f858129d7
Abhinav Shinow <abhinavshinow@gmail.com>
2021-09-25 20:07:31 +0530

    Added Method to clean the URL

--- Preprocessing/extractor.py
+++ Preprocessing/extractor.py
@@ -1,2 +1,15 @@
-#def extractor():
+def clean(token):
+    tokensBySlash = str(input.encode('utf-8')).split('/')
+    allTokens=[]
+    for i in tokensBySlash:
+        tokens = str(i).split('-')
+        tokensByDot = []
+        for j in range(0,len(tokens)):
+            tempTokens = str(tokens[j]).split('.')
+            tokentsByDot = tokensByDot + tempTokens
+        allTokens = allTokens + tokens + tokensByDot
+    allTokens = list(set(allTokens))
+    if 'com' in allTokens:
+        allTokens.remove('com')
+    return allTokens
     
