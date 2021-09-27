8ea1f97f1a7fcf33033c2f2311fee62eb0ded8ff
Abhinav Shinow <abhinavshinow@gmail.com>
2021-09-27 23:33:05 +0530

    Feature extractor class updated

--- Preprocessing/extractor.py
+++ Preprocessing/extractor.py
@@ -1,7 +1,36 @@
+#Current plan is to map the features to a dictioanary and then convert it to a csv file.
+# Feauture extraction 
 class feature_extractor(object):
     def __init__(self,url):
         self.url=url
-        self.domain=url.split('//')[-1].split('/')[0]
+        self.length=len(url)
+        #self.domain=url.split('//')[-1].split('/')[0]
+    #def entropy(self):
+    
+    def num_digits(self):
+        return sum(n.isdigit() for n in self.url)
+    
+    def num_char(self):
+        return sum(n.alpha() for n in self.url)
+    
+    def has_http(self):
+        if "http" in self.url:
+            return 1
+        else:
+            return 0
+    
+    def has_https(self):
+        if "https" in self.url:
+            return 1
+        else:
+            return 0
+    
+    #def num_special_char(self):
+    #
+    
+    #def num
+
+
 
 def clean(input):
     tokensBySlash = str(input.encode('utf-8')).split('/')
